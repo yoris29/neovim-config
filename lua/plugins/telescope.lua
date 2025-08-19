0340811621d2b92fbb7ecfb41228d92d6c4fcb06
yoris29 <ydoumi29@gmail.com>
2025-08-19 17:46:16 +0100

    Added autoformating and startup screen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,25 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        require('telescope').setup({
-            defaults = {
-                file_ignore_patterns = { "node_modules", "%.git" },
-            }
-        })
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local builtin = require('telescope.builtin')
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					"%.git",
+					"src/generated/prisma/.*", -- ignore all generated client files
+					"prisma/migrations/.*", -- SQL migrations
+					"prisma/generated/.*", -- other generated code
+				}
+
+
+			}
+		})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	end
 }
